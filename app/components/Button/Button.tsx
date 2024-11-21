119bf7fc46eca17555dea25c0ffa96371456377f
sahil <sahilkacchadiya@gmail.com>
2024-11-21 19:33:22 +0530

    add button

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,58 @@
+import React, { ButtonHTMLAttributes, ReactNode } from 'react'
+import classes from './button.module.css'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant | string
+  children: ReactNode
+  leftAdornment?: ReactNode
+  rightAdornment?: ReactNode
+  className?: string
+  iconClassName?: string
+}
+
+export enum ButtonVariant {
+  PRIMARY = 'primary',
+  OUTLINE = 'outline',
+  TEXT = 'text'
+}
+
+const Button: React.FC<ButtonProps> = ({
+  variant = 'primary',
+  className,
+  children,
+  leftAdornment,
+  rightAdornment,
+  disabled = false,
+  ...props
+}) => {
+  const renderButtonVariant = () => {
+    switch (variant) {
+      case ButtonVariant.OUTLINE:
+        return classes.secondary
+      case ButtonVariant.TEXT:
+        return classes.text
+      default:
+        return classes.PRIMARY
+    }
+  }
+  return (
+    <>
+      {!!leftAdornment && (
+        <span className={`{iconClassName}`}>{leftAdornment}</span>
+      )}
+      <button
+        className={`flex items-center justify-center py-2 px-4 rounded ${
+          disabled ? 'bg-blue-300 cursor-not-allow' : renderButtonVariant()
+        } ${className}`}
+        {...props}
+      >
+        {children}
+      </button>
+      {!!rightAdornment && (
+        <span className={`{iconClassName}`}>{rightAdornment}</span>
+      )}
+    </>
+  )
+}
+
+export default Button
